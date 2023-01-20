41d808f6eff7b159dd36ded095026d92f25aee9c
Luan E <luan.erazo@skyrocketlab.space>
2023-01-20 18:17:47 -0500

    atrreglo de la barra inferior y la barra de firefox

--- wp-content/themes/goddess_shape/assets/js/main.js
+++ wp-content/themes/goddess_shape/assets/js/main.js
@@ -1,31 +1,28 @@
-import { throttle } from "lodash";
-// window.addEventListener("resize", function () {
-//     let innerHeight = window.innerHeight;
-//     let isNavbarVisible = window.innerHeight < window.outerHeight;
-//     // aquí puedes aplicar la regla css para posicionar la barra de navegación
-//     let tabBar = document.getElementById('tab_bar');
-//     console.log(innerHeight);
-//     console.log('outter ->');
-//     console.log(window.outerHeight);
-//     if(isNavbarVisible) {
-//         tabBar.classList.remove("tab_bar_navbar");
-//     } else {
-//         tabBar.classList.add("tab_bar_navbar");
-//     }
-// });
+// import { throttle } from "lodash";
+window.addEventListener("resize", function () {
+    let innerHeight = window.innerHeight;
+    let isNavbarVisible = window.innerHeight < window.outerHeight;
+    // aquí puedes aplicar la regla css para posicionar la barra de navegación
+    let tabBar = document.getElementById('tab_bar');
+    if(isNavbarVisible) {
+        tabBar.classList.remove("tab_bar_navbar");
+    } else {
+        tabBar.classList.add("tab_bar_navbar");
+    }
+});
 
-function checkNavbar() {
-  let tabBar = document.getElementById("tab_bar");
-  let isNavbarVisible = window.innerHeight < window.outerHeight;
-  // aquí puedes aplicar la regla css para posicionar la barra de navegación
-  console.log(window.innerHeight);
-  //   console.log('outter ->');
-  //   console.log(window.outerHeight);
-  if (isNavbarVisible) {
-    tabBar.classList.remove("tab_bar_navbar");
-  } else {
-    tabBar.classList.add("tab_bar_navbar");
-  }
-  requestAnimationFrame(checkNavbar);
-}
-window.addEventListener("scroll", throttle(checkNavbar, 100));
+// function checkNavbar() {
+//   let tabBar = document.getElementById("tab_bar");
+//   let isNavbarVisible = window.innerHeight < window.outerHeight;
+//   // aquí puedes aplicar la regla css para posicionar la barra de navegación
+//   console.log(window.innerHeight);
+//   //   console.log('outter ->');
+//   //   console.log(window.outerHeight);
+//   if (isNavbarVisible) {
+//     tabBar.classList.remove("tab_bar_navbar");
+//   } else {
+//     tabBar.classList.add("tab_bar_navbar");
+//   }
+//   requestAnimationFrame(checkNavbar);
+// }
+// window.addEventListener("scroll", window.lodash.throttle(checkNavbar, 100));
--- wp-content/themes/goddess_shape/functions.php
+++ wp-content/themes/goddess_shape/functions.php
@@ -20,9 +20,9 @@ function init_template()
     wp_enqueue_style('styles', get_stylesheet_directory_uri() . '/style.css', 'tailwind', '1.0', 'all');
 
     // ---------------------- Register Scripts ----------------------
-    wp_enqueue_script('lodash', 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js', 'lodash', '1.0', 'all');
+    wp_enqueue_script('lodash', 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js', array(), '4.17.21', true);
     wp_enqueue_script('iconify', 'https://code.iconify.design/iconify-icon/1.0.0-beta.2/iconify-icon.min.js', '', '1.0', 'all');
-    wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/js/main.js', ['slickjs', 'lodash', 'iconify'], '1.8.1', 'all');
+    wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/js/main.js', ['lodash', 'iconify'], '1.8.1', 'all');
 }
 add_action('after_setup_theme', 'init_template');
 ?>
