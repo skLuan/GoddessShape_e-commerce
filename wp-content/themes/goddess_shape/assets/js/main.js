30aa05a89c082a677b40d13b895dfcf190e94619
Luan E <luan.erazo@skyrocketlab.space>
2023-01-20 17:50:39 -0500

    lodash by cdn and scroll event

--- wp-content/themes/goddess_shape/assets/js/main.js
+++ wp-content/themes/goddess_shape/assets/js/main.js
@@ -1,9 +1,31 @@
+import { throttle } from "lodash";
+// window.addEventListener("resize", function () {
+//     let innerHeight = window.innerHeight;
+//     let isNavbarVisible = window.innerHeight < window.outerHeight;
+//     // aquí puedes aplicar la regla css para posicionar la barra de navegación
+//     let tabBar = document.getElementById('tab_bar');
+//     console.log(innerHeight);
+//     console.log('outter ->');
+//     console.log(window.outerHeight);
+//     if(isNavbarVisible) {
+//         tabBar.classList.remove("tab_bar_navbar");
+//     } else {
+//         tabBar.classList.add("tab_bar_navbar");
+//     }
+// });
 
-let isNavbarVisible = window.innerHeight < window.outerHeight;
-
-let tabBar = document.getElementById('tab_bar');
-if(isNavbarVisible) {
+function checkNavbar() {
+  let tabBar = document.getElementById("tab_bar");
+  let isNavbarVisible = window.innerHeight < window.outerHeight;
+  // aquí puedes aplicar la regla css para posicionar la barra de navegación
+  console.log(window.innerHeight);
+  //   console.log('outter ->');
+  //   console.log(window.outerHeight);
+  if (isNavbarVisible) {
     tabBar.classList.remove("tab_bar_navbar");
-} else {
+  } else {
     tabBar.classList.add("tab_bar_navbar");
+  }
+  requestAnimationFrame(checkNavbar);
 }
+window.addEventListener("scroll", throttle(checkNavbar, 100));
