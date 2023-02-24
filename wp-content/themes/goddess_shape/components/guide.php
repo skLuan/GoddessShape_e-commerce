87ff68859dd3b8145e81c7acb7334f7436d9280f
Luan E <luan.erazo@skyrocketlab.space>
2023-02-23 20:02:22 -0500

    arreglando paths

--- wp-content/themes/goddess_shape/components/guide.php
+++ wp-content/themes/goddess_shape/components/guide.php
@@ -4,7 +4,7 @@
     <div class="grid grid-cols-2 lg:col-span-7 gap-5 items-center justify-center w-full h-64 bg-white px-5 mb-10">
         <div class="relative flex flex-col px-8">
             <h2 class="flex my-2 font-champagne_limousines text-lg font-bold text-center mx-auto text-red-soft">Acabado 1</h2>
-            <img class="absolute left-3 -rotate-45 -top-3 z-0 opacity-30" width="90%" src="<?= IMAGE . 'home/sol_sin_centro.png' ?>" alt="">
+            <img class="absolute left-3 -rotate-45 -top-3 z-0 opacity-30" width="90%" src="<?= IMAGE . 'Home/sol_sin_centro.png' ?>" alt="">
 
             <div class="flex z-10 relative flex-row h-28 w-28 bg-gray-card rounded-full mx-auto mb-2"></div>
             <h2 class="flex ml-2 text-lg lg:font-normal leading-5 font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
@@ -12,7 +12,7 @@
         </div>
         <div class="relative flex flex-col px-8">
             <h2 class="flex my-2 font-champagne_limousines text-lg font-bold text-center mx-auto text-red-soft">Acabado 2</h2>
-            <img class="absolute left-3 -rotate-45 -top-3 z-0 opacity-30" width="90%" src="<?= IMAGE . 'home/sol_sin_centro.png' ?>" alt="">
+            <img class="absolute left-3 -rotate-45 -top-3 z-0 opacity-30" width="90%" src="<?= IMAGE . 'Home/sol_sin_centro.png' ?>" alt="">
 
             <div class="flex relative z-10 h-28 w-28 bg-gray-card rounded-full mx-auto mb-2"></div>
             <h2 class="flex ml-2 text-lg lg:font-normal leading-5 font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
--- wp-content/themes/goddess_shape/components/loops/loop-discount.php
+++ wp-content/themes/goddess_shape/components/loops/loop-discount.php
@@ -1,6 +1,6 @@
 <section class="flex flex-col text-center relative">
     <picture class="absolute opacity-30 z-0  w-full flex">
-        <img class=" mx-auto rotate-45" width="250px" src="<?= get_stylesheet_directory_uri() ?>/assets/img/home/sol_sin_centro.png" alt="">
+        <img class=" mx-auto rotate-45" width="250px" src="<?= IMAGE ?>Home/sol_sin_centro.png" alt="">
     </picture>
     <h1 class="bg-white font-champagne_limousines text-[28px] text-red-soft leading-5 w-full font-bold p-4 pt-16 ">En descuento</h1>
     <img class="mx-auto mb-5" width="44px" src="<?= get_stylesheet_directory_uri() ?>/assets/img/discount-icon.svg" alt="">
