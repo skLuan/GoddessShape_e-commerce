d01d80e8d66eb647004a7692cf48b1ceed7a5a0e
Jose Miguel Ayala <75082248+Josmiaya@users.noreply.github.com>
2023-04-03 17:26:06 -0500

    estilo inicial de blog, botones de front con interaccion para filtrar por categoria

--- wp-content/themes/goddess_shape/assets/js/main.js
+++ wp-content/themes/goddess_shape/assets/js/main.js
@@ -89,3 +89,38 @@ try {
 } catch (error) {
   
 }
+
+const buttons = document.querySelectorAll('.button-filter');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const category = button.dataset.category;
+        const currentUrl = new URL(window.location.href);
+        currentUrl.searchParams.set('category', category);
+        window.location.href = currentUrl.href;
+    });
+});
+
+const filterButtons = document.querySelectorAll(".button-filter");
+
+// Agregar un controlador de eventos para cada botón
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Remover la clase "active" de todos los botones
+    filterButtons.forEach((button) => button.classList.remove("active"));
+
+    // Agregar la clase "active" al botón que se hizo clic
+    button.classList.add("active");
+
+    // Obtener el valor del atributo "data-category" del botón que se hizo clic
+    const filterValue = button.getAttribute("data-category");
+
+    // Mostrar u ocultar los productos según el filtro seleccionado
+    filterProducts(filterValue);
+  });
+});
+
+
+// Show all products on page load
+filterProducts('all');
+document.querySelector('.button-filter[data-category="all"]').classList.add('active');
